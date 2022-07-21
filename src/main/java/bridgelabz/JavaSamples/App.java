005f7413e36d50d4099e8e29536223108ac0f1a2
ranirban60 <64977972+ranirban60@users.noreply.github.com>
2022-07-21 21:39:10 +0530

    Update App.java

--- src/main/java/bridgelabz/JavaSamples/App.java
+++ src/main/java/bridgelabz/JavaSamples/App.java
@@ -13,10 +13,10 @@ public class App
     public static void main( String[] args )
     {
        String message = "Hello World !";
-       LOG.debug(message + " Will Be Printed On Debug 1");
-       LOG.info(message + " Will Be Printed On Info 1");
-       LOG.warn(message + " Will Be Printed On Warn 1");
-       LOG.error(message + " Will Be Printed On Error 1");
+       LOG.debug(message + " Will Be Printed On Debug 3");
+       LOG.info(message + " Will Be Printed On Info 3");
+       LOG.warn(message + " Will Be Printed On Warn 3");
+       LOG.error(message + " Will Be Printed On Error 3");
        LOG.fatal(message + " Will Be Printed On Fatal 1");
        LOG.info("Appending string: {}.", message);
        System.out.println( "Hello " );
